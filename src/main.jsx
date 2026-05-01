d85c02cb2bd090f725d0172138b2c177407a6841
Alfredo Morales <161681002+aljmorales@users.noreply.github.com>
2026-04-30 20:36:28 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
-import { BrowserRouter } from 'react-router-dom';
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import { HashRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
-)
+  </HashRouter>,
+);
